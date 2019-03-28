e52bacacfb9355e26fd90347ca49533a614edf36
Edward Lee <edlee1@stanford.edu>
2019-03-27 17:15:36 -0700

    moved to androidx

--- app/src/main/java/com/edwardlee259/reflectapp/ui/MainActivity.kt
+++ app/src/main/java/com/edwardlee259/reflectapp/ui/MainActivity.kt
@@ -1,13 +1,13 @@
 package com.edwardlee259.reflectapp.ui
 
 import android.os.Bundle
-import android.support.design.widget.NavigationView
-import android.support.v4.view.GravityCompat
-import android.support.v7.app.ActionBarDrawerToggle
-import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import com.edwardlee259.reflectapp.R
+import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
@@ -48,7 +48,9 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         when (item.itemId) {
-            R.id.action_settings -> return true
+            R.id.action_settings -> {
+                return true
+            }
             else -> return super.onOptionsItemSelected(item)
         }
     }
